f4f4d79689b3540efb6fc589dbb4862a239cfd86
Eugene <eugene@MacBook-Pro-Eugene.local>
2025-03-24 21:08:23 +0300

    Add base model and swagger

--- test_task/settings.py
+++ test_task/settings.py
@@ -20,7 +20,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_spectacular',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
